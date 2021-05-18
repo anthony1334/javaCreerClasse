c372713d1a61f5c5019496cc0c1a675a43e4fcb1
Stephan Anthony <anthony.stephan@gmail.com>
2021-05-18 15:01:07 +0200

    Refactorage

--- src/fr/diginamic/recensement/Menu.java
+++ src/fr/diginamic/recensement/Menu.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Menu extends MenuService{
     public void traiter(Recensement recensement, Scanner scanner){
+        MenuService service = null;
         int choix = 0;
         while (choix != 9){
             System.out.println(" 1: Population d\'une ville donnée \n 2: Population d\'un département donné\n 3: Population d\'une région donnée\n 4: Afficher les 10 régions les plus peuplées\n 5: Afficher les 10 départements les plus peuplés\n 6:  Afficher les 10 villes les plus peuplées d\'un département\n 7:  Afficher les 10 villes les plus peuplées d\'une région\n 8:  Afficher les 10 villes les plus peuplées de FRANCE  ");
@@ -11,44 +12,47 @@ public class Menu extends MenuService{
             try {
                 choix = Integer.parseInt(scanner.nextLine());
 
-                    switch (choix){
-                        case 1 :
-                            RecherchePopulationVille rechercheVille = new RecherchePopulationVille();
-                            rechercheVille.traiter(recensement, scanner);
-                            break;
-                        case 2 :
-                            RecherchePopulationDepartement recherchedepartement = new RecherchePopulationDepartement();
-                            recherchedepartement.traiter(recensement, scanner);
-                            break;
-                        case 3 :
-                            RecherchePopulationRegion rechercheRegion = new RecherchePopulationRegion();
-                            rechercheRegion.traiter(recensement,scanner);
-                            break;
-                        case 4:
-                            Top10Region top10Region = new Top10Region();
-                            top10Region.traiter(recensement, scanner);
-                            break;
-                        case 5:
-                            Top10Departement top10Departement = new Top10Departement();
-                            top10Departement.traiter(recensement,scanner);
-                            break;
-                        case 6:
-                            Top10VilleParDepartement top10VilleParDepartement = new Top10VilleParDepartement();
-                            top10VilleParDepartement.traiter(recensement,scanner);
-                            break;
-                        case 7:
-                            Top10VilleParRegion top10VilleParRegion = new Top10VilleParRegion();
-                            top10VilleParRegion.traiter(recensement,scanner);
-                            break;
-                        case 8:
-                            Top10VillesDeFrance top10VillesDeFrance = new Top10VillesDeFrance();
-                            top10VillesDeFrance.traiter(recensement,scanner);
-                            break;
-                        case 9:
-                            break;
-                        default:
-                            System.out.println("Veuillez entrez un nombre valid.");
-                        }
+                switch (choix){
+                    case 1 :
+                       service = new RecherchePopulationVille();
+
+                        break;
+                    case 2 :
+                        service  = new RecherchePopulationDepartement();
+
+                        break;
+                    case 3 :
+                        service = new RecherchePopulationRegion();
+
+                        break;
+                    case 4:
+                        service = new Top10Region();
+
+                        break;
+                    case 5:
+                        service  = new Top10Departement();
+
+                        break;
+                    case 6:
+                        service  = new Top10VilleParDepartement();
+
+                        break;
+                    case 7:
+                        service  = new Top10VilleParRegion();
+
+                        break;
+                    case 8:
+                        service  = new Top10VillesDeFrance();
+                        break;
+                    case 9:
+                        service = null;
+                        break;
+                    default:
+                        System.out.println("Veuillez entrez un nombre valid.");
+                    }
+                    if(service != null){
+                        service.traiter(recensement, scanner);
+                    }
 
 
                 }catch (NumberFormatException e){
